38c5a93dc7645bdb236f0de7916faee3b170a9e7
Jason Wiker <jason.wiker@gmail.com>
2020-09-01 16:33:42 -0600

    Stub Empty test for missile

--- BattleshipTests/BoardTest.cs
+++ BattleshipTests/BoardTest.cs
@@ -49,5 +49,24 @@ namespace BattleshipTests
             this.board.PlaceItem("B", "3", "x", "🚢");
             Assert.Equal(this.board.ReturnBoard, testBoard);
         }
+
+        [Fact]
+        public void shouldPlaceMissile()
+        {
+            Dictionary<string, Dictionary<string, string>> testBoard = new Dictionary<string, Dictionary<string, string>>() {
+                { "A", new Dictionary<string, string>() { { "1", "*" }, { "3", "*" }, { "2", "*" }, { "5", "*" }, { "4", "*" }, { "7", "*" }, { "6", "*" }, { "8", "*" } } },
+                { "B", new Dictionary<string, string>() { { "1", "*" }, { "3", "⭕" }, { "2", "*" }, { "5", "*" }, { "4", "*" }, { "7", "*" }, { "6", "*" }, { "8", "*" } } },
+                { "C", new Dictionary<string, string>() { { "1", "*" }, { "3", "*" }, { "2", "*" }, { "5", "*" }, { "4", "*" }, { "7", "*" }, { "6", "*" }, { "8", "*" } } },
+                { "D", new Dictionary<string, string>() { { "1", "*" }, { "3", "*" }, { "2", "*" }, { "5", "*" }, { "4", "*" }, { "7", "*" }, { "6", "*" }, { "8", "*" } } },
+                { "E", new Dictionary<string, string>() { { "1", "*" }, { "3", "*" }, { "2", "*" }, { "5", "*" }, { "4", "*" }, { "7", "*" }, { "6", "*" }, { "8", "*" } } },
+                { "F", new Dictionary<string, string>() { { "1", "*" }, { "3", "*" }, { "2", "*" }, { "5", "*" }, { "4", "*" }, { "7", "*" }, { "6", "*" }, { "8", "*" } } },
+                { "G", new Dictionary<string, string>() { { "1", "*" }, { "3", "*" }, { "2", "*" }, { "5", "*" }, { "4", "*" }, { "7", "*" }, { "6", "*" }, { "8", "*" } } },
+                { "H", new Dictionary<string, string>() { { "1", "*" }, { "3", "*" }, { "2", "*" }, { "5", "*" }, { "4", "*" }, { "7", "*" }, { "6", "*" }, { "8", "*" } } }
+            };
+            Console.WriteLine(this.board.ReturnBoard["B"]);
+
+            this.board.PlaceMissile("B", "3", "x", "⭕");
+            Assert.Equal(this.board.ReturnBoard, testBoard);
+        }
     }
 }
